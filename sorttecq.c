5ed4abc04384c914f982a794babeb5f262888dc6
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-02-12 16:34:17 +0530

    Create sorttecq.c

---
+++ sorttecq.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,t,a[50];
+printf("enter the array Range ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("\n Sorted array is given %d",a[i]);
+}
+return 0;
+}
